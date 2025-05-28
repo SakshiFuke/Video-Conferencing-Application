0a8a7635c876484c1a5e2f4138af7bc124b48a5d
SakshiFuke <142399426+SakshiFuke@users.noreply.github.com>
2025-05-28 16:54:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
+import VideoCall from './components/VideoCall';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/video-call" element={<VideoCall />} />
+      </Routes>
+    </Router>
+    
+  );
+};
+
+export default App;
